d7f3b0cc94862f4d71c71484668da46569d8f93c
Al Farhaan khan I Inamdar <63239385+Zouziszzm@users.noreply.github.com>
2023-10-24 07:56:49 +0530

    clear working
    Issue: year not visible.

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://farhaan:PHMH52ku3VA4oCsp@bookstore.lqu0kuz.mongodb.net/test?retryWrites=true&w=majority"
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
--- backend/statics.js
+++
@@ -1,2 +0,0 @@
-export const conStream = "mongodb+srv://farhaan:PHMH52ku3VA4oCsp@bookstore.lqu0kuz.mongodb.net/test?retryWrites=true&w=majority";
-
